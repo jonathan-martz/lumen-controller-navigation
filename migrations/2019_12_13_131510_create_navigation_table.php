6d706f42403fea338cc297d5e046f508ffa58e48
jonathan-martz <github@jmartz.de>
2019-12-14 22:24:56 +0100

    add migration for navigation

---
+++ migrations/2019_12_13_131510_create_navigation_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNavigationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('navigation', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('link');
+            $table->string('icon');
+            $table->string('show_user');
+            $table->string('show_guest');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('navigation');
+    }
+}
